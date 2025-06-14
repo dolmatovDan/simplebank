13daa9d9e9217a64264e92569c202135d39ab11e
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2025-06-14 22:05:53 +0300

    add unit tests

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,1 +1,63 @@
 package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/dolmatovDan/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, account *Account) *Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomInt(1, 100),
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), &arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t, createRandomAccount(t))
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t, createRandomAccount(t))
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.Equal(t, entry1.CreatedAt, entry2.CreatedAt)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit: 5,
+		Offset: 5,
+	}
+	for range 10 {
+		createRandomEntry(t, account)
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), &arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entries)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.NotZero(t, entry.AccountID)
+		require.NotZero(t, entry.Amount)
+	}
+}
